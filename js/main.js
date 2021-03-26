48962297b098ce1d2ee0d731150ab756c2c7caca
Learning-to-code12 <66002514+Learning-to-code12@users.noreply.github.com>
2021-03-26 12:37:34 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,62 +1,36 @@
 
-(function ($) {
-    "use strict";
 
-    
-    /*==================================================================
-    [ Validate ]*/
-    var name = $('.validate-input input[name="name"]');
-    var email = $('.validate-input input[name="email"]');
-    var subject = $('.validate-input input[name="subject"]');
-    var message = $('.validate-input textarea[name="message"]');
+(function($) {
 
 
-    $('.validate-form').on('submit',function(){
-        var check = true;
 
-        if($(name).val().trim() == ''){
-            showValidate(name);
-            check=false;
+  $('#register-form').validate({
+    rules : {
+        first_name : {
+            required: true,
+        },
+        last_name : {
+            required: true,
+        },
+        email : {
+            required: true,
+            email : true
+        },
+        phone_number : {
+            required: true,
         }
+    },
+    onfocusout: function(element) {
+        $(element).valid();
+    },
+});
 
-        if($(subject).val().trim() == ''){
-            showValidate(subject);
-            check=false;
-        }
+    jQuery.extend(jQuery.validator.messages, {
+        required: "",
+        remote: "",
+        email: "",
+        number: "",
+        digits: "",
 
-
-        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-            showValidate(email);
-            check=false;
-        }
-
-        if($(message).val().trim() == ''){
-            showValidate(message);
-            check=false;
-        }
-
-        return check;
     });
-
-
-    $('.validate-form .input1').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
-       });
-    });
-
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-
-        $(thisAlert).removeClass('alert-validate');
-    }
-    
-    
-
 })(jQuery);
